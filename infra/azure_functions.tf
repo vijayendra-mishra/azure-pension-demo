7861c0c7f0ab8af6d25993dce09c8e7c8a31a84b
Vijayendra Mishra <mishra.vijayendra.v@outlook.com>
2025-09-10 21:58:12 +0100

    feat: migrate to shared service plan

--- infra/azure_functions.tf
+++ infra/azure_functions.tf
@@ -1,15 +1,23 @@
 resource "azurerm_resource_group" "pension" {
   name     = "vjs-pension-demo-rg"
   location = var.location
+
+  tags = {
+    "azd-env-name" = var.environment
+  }
 }
 
 resource "azurerm_storage_account" "pension" {
-  name                     = "vjspensiondemo"
+  name                     = "vjspensiondemo${var.environment}"
   resource_group_name      = azurerm_resource_group.pension.name
   location                 = azurerm_resource_group.pension.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
+
+  tags = {
+    "azd-env-name" = var.environment
+  }
 }
 
 resource "azurerm_application_insights" "pension" {
@@ -17,21 +25,30 @@ resource "azurerm_application_insights" "pension" {
   location            = azurerm_resource_group.pension.location
   resource_group_name = azurerm_resource_group.pension.name
   application_type    = "web"
+
+  tags = {
+    "azd-env-name" = var.environment
+  }
 }
 
-resource "azurerm_service_plan" "pension" {
-  name                = "vjs-pension-${var.environment}-plan"
+# Shared App Service Plan for both dev and prod
+resource "azurerm_service_plan" "pension_shared" {
+  name                = "vjs-pension-shared-plan"
   location            = azurerm_resource_group.pension.location
   resource_group_name = azurerm_resource_group.pension.name
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "B1"  # Basic tier for shared plan
+
+  tags = {
+    "azd-env-name" = var.environment
+  }
 }
 
 resource "azurerm_linux_function_app" "pension" {
   name                = "vjs-pension-${var.environment}-func"
   location            = azurerm_resource_group.pension.location
   resource_group_name = azurerm_resource_group.pension.name
-  service_plan_id     = azurerm_service_plan.pension.id
+  service_plan_id     = azurerm_service_plan.pension_shared.id
 
   storage_account_name       = azurerm_storage_account.pension.name
   storage_account_access_key = azurerm_storage_account.pension.primary_access_key
