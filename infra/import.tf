71b06b93bc8403bf115150632d55a817d68f3930
Vijayendra Mishra <mishra.vijayendra.v@outlook.com>
2025-09-10 20:32:15 +0100

    feat: Add import block for existing resource group
    - Add import.tf to automatically import existing resource group
    - This will resolve the resource already exists error in Terraform Cloud
    - Enables production infrastructure deployment

---
+++ infra/import.tf
@@ -0,0 +1,5 @@
+# Import existing resource group for production environment
+import {
+  to = azurerm_resource_group.pension
+  id = "/subscriptions/780e8e76-e1c2-412d-b6d3-cc253736e07d/resourceGroups/vjs-pension-demo-rg"
+}
